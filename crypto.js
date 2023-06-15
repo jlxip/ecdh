cb0e90d77c92b58c1755921787486dbeca2ce554
jlxip <jlxip@protonmail.com>
2023-06-15 19:46:22 +0200

    HKDF

--- crypto.js
+++ crypto.js
@@ -20,7 +20,7 @@ var CRYPTO = {
         restoreKey: function (priv) {
             this._key = this._ec.keyFromPrivate(priv);
         },
-        // Key derivation function, from alice's public key
+        // Key derivation, from Alice's public key
         derive: function (alice) {
             let other;
             try {
@@ -30,7 +30,17 @@ var CRYPTO = {
                 throw new Error('Invalid key');
             }
 
-            return this._key.derive(other).toString(16);
+            // Diffie-Hellman shared secret
+            const shared = this._key.derive(other);
+            // Turn this BigNumber into a sjcl bitArray
+            // SJCL's bitArray is an array of 4-byte words
+            // shared.words is actually an array of 26-bit words
+            // That's a very deep pitfall right there
+            const libchange = sjcl.codec.hex.toBits(shared.toString(16));
+            // HKDF: no salt, no info, default hash (SHA-256)
+            const result = hkdf(libchange, 256, [], []);
+
+            return sjcl.codec.hex.fromBits(result);
         }
     },
 
@@ -81,4 +91,42 @@ var CRYPTO = {
             return sjcl.codec.utf8String.fromBits(dec);
         }
     }
+};
+
+// EXTRA: Apparently SJCL is compiled without support for HKDF for no reason
+// https://github.com/bitwiseshiftleft/sjcl/issues/396
+// This is a copy-paste of sjcl's HKDF
+// https://github.com/bitwiseshiftleft/sjcl/blob/1.0.8/core/hkdf.js
+let hkdf = function (ikm, keyBitLength, salt, info, Hash) {
+  var hmac, key, i, hashLen, loops, curOut, ret = [];
+
+  Hash = Hash || sjcl.hash.sha256;
+  if (typeof info === "string") {
+    info = sjcl.codec.utf8String.toBits(info);
+  }
+  if (typeof salt === "string") {
+    salt = sjcl.codec.utf8String.toBits(salt);
+  } else if (!salt) {
+    salt = [];
+  }
+
+  hmac = new sjcl.misc.hmac(salt, Hash);
+  key = hmac.mac(ikm);
+  hashLen = sjcl.bitArray.bitLength(key);
+
+  loops = Math.ceil(keyBitLength / hashLen);
+  if (loops > 255) {
+    throw new sjcl.exception.invalid("key bit length is too large for hkdf");
+  }
+
+  hmac = new sjcl.misc.hmac(key, Hash);
+  curOut = [];
+  for (i = 1; i <= loops; i++) {
+    hmac.update(curOut);
+    hmac.update(info);
+    hmac.update([sjcl.bitArray.partial(8, i)]);
+    curOut = hmac.digest();
+    ret = sjcl.bitArray.concat(ret, curOut);
+  }
+  return sjcl.bitArray.clamp(ret, keyBitLength);
 };
